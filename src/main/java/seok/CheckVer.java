6929597febb0ed4abdda2d57a2a51a37360e4841
Lseoksee <da864268@naver.com>
2023-06-07 00:34:40 +0900

    CheckVer readAllBytes 대체

--- src/main/java/seok/CheckVer.java
+++ src/main/java/seok/CheckVer.java
@@ -1,17 +1,20 @@
 package seok;
 
 import java.awt.Desktop;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 import javax.swing.JOptionPane;
 
 import org.json.JSONObject;
 
-public class CheckVer extends Main implements Runnable  {
-    
+public class CheckVer extends Main implements Runnable {
+
     @Override
     public void run() {
         /* 깃허브 api를 사용한 버전확인 방법 */
@@ -21,12 +24,12 @@ public class CheckVer extends Main implements Runnable  {
             // httprequset 연결부분
 
             InputStream is = huc.getInputStream();
-            JSONObject jsonObject = new JSONObject(new String(is.readAllBytes(), "utf-8"));
-
+            JSONObject jsonObject = new JSONObject(
+                    new BufferedReader(new InputStreamReader(is)).lines().collect(Collectors.joining("\n")));
             String onver = jsonObject.getString("tag_name");
 
             if (!onver.equals(Main.RESVER)) {
-                String mesege = "업데이트가 있습니다!\n현재버전: "+ Main.RESVER+"\n최신버전: "+onver;
+                String mesege = "업데이트가 있습니다!\n현재버전: " + Main.RESVER + "\n최신버전: " + onver;
                 int state = JOptionPane.showConfirmDialog(fr, mesege, "업데이트 알림", JOptionPane.OK_CANCEL_OPTION);
                 if (state == 0) {
                     Desktop.getDesktop().browse(new URI(jsonObject.getString("html_url")));
