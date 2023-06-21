b39dc8404d14e956e6b24739a87d51dc06541943
Lseoksee <da864268@naver.com>
2023-06-21 14:49:11 +0900

    임시포트 개방하는 법

--- src/main/java/seok/test/porttest.java
+++ src/main/java/seok/test/porttest.java
@@ -1,11 +1,30 @@
 package seok.test;
 
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+
 public class porttest {
-    public static void main(String[] args) {
-        try (Socket socket = new Socket()){
-            socket.connect(new InetSocketAddress("58.232.252.163", 25565), 2000);
+        public static void main(String[] args) {
+        try {
+            ServerSocket svs = new ServerSocket(25565); //테스트로 오픈할 포트
+            new Thread(new isopen()).start();
+            svs.accept();
+            svs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class isopen implements Runnable {
+
+    @Override
+    public void run() {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("58.232.252.164", 25565), 2000);   
+            //오픈된 포트를 외부IP주소로 연결하여 포트포워딩이 정상적으로 되었는지를 확인
             System.out.println("성공적으로 연결됨");
         } catch (Exception e) {
             e.printStackTrace();
