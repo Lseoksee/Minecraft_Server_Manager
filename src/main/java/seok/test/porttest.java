4f177e7a61bb90c741f57fbc604e2e36f962629d
Lseoksee <da864268@naver.com>
2023-06-21 11:41:09 +0900

    Socket timeout 테스트

--- src/main/java/seok/test/porttest.java
+++ src/main/java/seok/test/porttest.java
@@ -1,11 +1,14 @@
 package seok.test;
 
+import java.net.InetSocketAddress;
 import java.net.Socket;
 public class porttest {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 25565)){
+        try (Socket socket = new Socket()){
+            socket.connect(new InetSocketAddress("58.232.252.163", 25565), 2000);
             System.out.println("성공적으로 연결됨");
         } catch (Exception e) {
+            e.printStackTrace();
             System.out.println("포트포워딩 필요");
         }
     }
